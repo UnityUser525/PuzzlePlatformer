8a98873a928479723e9151de5a89dff33db78e2e
UnityUser525 <101831417+UnityUser525@users.noreply.github.com>
2022-04-20 15:09:31 -0500

    4/20/2022 - Moving Platform and Icicle bugfix

--- HamsterGame/Assets/Scripts/MovingObject.cs
+++ HamsterGame/Assets/Scripts/MovingObject.cs
@@ -15,6 +15,9 @@ public class MovingObject : MonoBehaviour
 
     public Vector2 targetPos;
 
+    private bool playerOnBool = false;
+    private GameObject playerOntop;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,8 +42,34 @@ public class MovingObject : MonoBehaviour
         moveToward();
     }
 
+    private float pastPosX = 0;
+    public float playerAdjust;
     private void moveToward()
     {
+        playerAdjust = transform.position.x - pastPosX;
+        pastPosX = transform.position.x;
+
+        if (playerOnBool == true)
+        {
+            playerOntop.transform.position = Vector2.MoveTowards(playerOntop.transform.position, new Vector2(targetPos.x + (playerOntop.transform.position.x - gameObject.transform.position.x), targetPos.y + (playerOntop.transform.position.y - gameObject.transform.position.y)), speed * Time.deltaTime);
+        }
         gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, targetPos, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2"))
+        {
+            playerOnBool = true;
+            playerOntop = collision.gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2"))
+        {
+            playerOnBool = false;
+        }
     }
 }
