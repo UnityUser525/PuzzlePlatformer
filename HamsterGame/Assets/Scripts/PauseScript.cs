543bae5e027c92446677db6f68ad27c423c948ca
UnityUser525 <101831417+UnityUser525@users.noreply.github.com>
2022-05-04 15:10:57 -0500

    5/4/2022

--- HamsterGame/Assets/Scripts/PauseScript.cs
+++ HamsterGame/Assets/Scripts/PauseScript.cs
@@ -7,17 +7,24 @@ public class PauseScript : MonoBehaviour
     public GameObject pauseMenu;
     private bool paused = false;
 
+    public GameObject pauseImage;
+
+    public Sprite pauseSprite;
+    public Sprite playSprite;
+
     public void pauseLevel()
     {
         if (paused == false)
         {
             pauseMenu.SetActive(true);
+            pauseImage.GetComponent<SpriteRenderer>().sprite = pauseSprite;
             Time.timeScale = 0;
             paused = true;
         }
         else if (paused == true)
         {
             pauseMenu.SetActive(false);
+            pauseImage.GetComponent<SpriteRenderer>().sprite = playSprite;
             Time.timeScale = 1;
             paused = false;
         }
